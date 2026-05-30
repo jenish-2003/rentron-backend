f1d4afc83ef5c1e7f8b35429d9921297f33a01a5
Balaji <balaji@calsoftgroup.com>
2026-05-30 11:14:46 +0530

    project setup

---
+++ src/main/java/com/example/rentron/RentronApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rentron/RentronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
